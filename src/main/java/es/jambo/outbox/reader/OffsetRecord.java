fe2bc088178c67221f02bb4ad84f7fbd8423882f
Juanma <jmsanchezmartagon@gmail.com>
2023-07-24 00:03:32 +0200

    wip. delete offsetrecord

--- src/main/java/es/jambo/outbox/reader/OffsetRecord.java
+++
@@ -1,9 +0,0 @@
-package es.jambo.outbox.reader;
-
-record OffsetRecord(String scn) {
-    OffsetRecord {
-        if (scn == null || scn.isBlank())
-            throw new IllegalArgumentException();
-    }
-}
-
